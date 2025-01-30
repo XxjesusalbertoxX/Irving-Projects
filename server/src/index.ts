407f48cf7da4b6accee21d3661edf8d8c7066234
XxjesusalbertoxX <villarrealperezjesusalberto@gmail.com>
2025-01-30 13:58:53 -0600

    docker completely function 100%

--- server/src/index.ts
+++ server/src/index.ts
@@ -27,12 +27,17 @@ app.use('/api/v1.0.0/auth', authRoutes);
 
 app.use('/api/helloworld', helloRoutes);
 
+console.log("🚀 Server is starting...");
+process.on("exit", (code) => console.log(`❌ Server exited with code ${code}`));
+process.on("SIGINT", () => console.log("❌ Server interrupted (SIGINT)"));
+process.on("SIGTERM", () => console.log("❌ Server terminated (SIGTERM)"));
+
 sequelize
   .authenticate()
   .then(() => console.log("✅ PostgreSQL Connected!"))
   .catch((err) => console.error("❌ Connection error:", err));
 
-// sequelize.sync({ alter: true }).then(() => {
-//   console.log('Database synced');
-//   app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
-// });
+sequelize.sync({ alter: true }).then(() => {
+  console.log('Database synced');
+  app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+ });
